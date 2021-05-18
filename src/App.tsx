30b644dfb651710a6f34daa394af5217e2ab444a
nonoakij <matsumoto_aki@camobile.com>
2021-05-18 13:44:26 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.VFC = () => (
-                <div className="App">
+  <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
